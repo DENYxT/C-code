ea43f7d50f98b5c1f6d58f938dd947ee48485c50
王凯潼 <November04-T@outlook.com>
2023-07-03 16:17:42 +0800

    简单的练习

---
+++ 初识C.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+void main()
+{
+	//printf("Hello,world\n");   //单行注释   /*  */多行注释 
+	
+	//int a, b, sum;  //定义了三个变量 
+	//a = 123;        //给变量赋值  “123” 
+	//b = 456;
+	//sum = a + b;    
+	//printf("sum is %d\n", sum);  //%d：代数作用 ，如果不写，后面的变量不会代入前面字符串 
+	
+	//int a;  
+	//a=99;
+	//float r;
+	//float s;
+	//r=5.365;
+	//s=r*r*3.1415926;
+	//printf("该圆的面积= %f\n",s);
+	//printf("%f;%f;%d;",r,s,a); 
+	 
+	 /* 
+	float a,b,c,ave;
+	a=5;
+	b=6;
+	c=7;
+	ave=(a+b+c)/3;
+	printf("平均值为：%f",ave);	
+	*/
+	
+	
+	//int score1,score2;
+	//scanf("%d%d",&score1,&score2);  //如果中间用  ，  隔开那么在你输入值得时候，两个值必须用  ， 隔开   其他符号同理 
+	//printf("score1=%d  score2=%d\n",score1,score2); 
+
+
+
+	//int n,m100,m10,m;	 
+	//printf("请输入一个三位数\n");
+	//scanf("%d",&n);
+	//m100=n/100;
+	//m10=n/10%10;
+	//m=n%10;
+	//n=m*100+m10*10+m100;
+	//printf("逆顺序之后的数%d",n); 
+
+
+	//char ch;
+	//ch=getchar();
+	//putchar(ch);
+	
+	
+	
+	//float f=3.1415926;
+	//printf("%6.4f",f);
+	
+	
+	int i;
+	scanf("%d",&i);
+	if(i>10)
+	{
+		printf("这是一个大于10的数\n");
+		printf("这个数是%d",i); 
+	} 
+	else
+	{
+		printf("这是一个小于10的数\n");
+		printf("这个数是%d",i); 
+	}
+	
+	
+	
+	
+
+
+
+
+	
+	
+	
+	
+	
+	
+	
+} 
